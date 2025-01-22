672e6a8cad2c2dbf40d73f31b414e3c66c224127
Ludwig Koch <lucky.koch@gmx.de>
2025-01-22 16:14:54 +0100

    Added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AboutComponent } from './about/about.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent, TimelineComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, TimelineComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
