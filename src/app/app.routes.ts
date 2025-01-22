7b5a2ab228d905f3a26d6d6c9a05448847257d45
Ludwig Koch <lucky.koch@gmx.de>
2025-01-22 16:33:49 +0100

    Added menu routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'timeline', component: TimelineComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '' }, // Redirect unknown routes to home
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
